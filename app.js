079c68c44a099ed58def3a43ef60fb921bd59bc2
aybaneze <andrea270696@gmail.com>
2018-09-25 05:46:02 -0500

    agregando habilidades

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit(); 
 });
 
+$('.dropdown-trigger').dropdown();
+$(document).ready(function(){
+  $('.slider').slider();
+});
+      
+
 // $(document).ready(function(){
 //   $('.parallax').parallax();
 // });
