79a73146d77a706f865285c6dd52329a80d48e8a
aybaneze <andrea270696@gmail.com>
2018-09-26 02:21:28 -0500

    agregando proyectos

--- app.js
+++ app.js
@@ -6,7 +6,29 @@ $('.dropdown-trigger').dropdown();
 $(document).ready(function(){
   $('.slider').slider();
 });
-      
+
+// $('.pushpin-demo-nav').each(function() {
+//   var $this = $(this);
+//   var $target = $('#' + $(this).attr('data-target'));
+//   $this.pushpin({
+//     top: $target.offset().top,
+//     bottom: $target.offset().top + $target.outerHeight() - $this.height()
+//   });
+// });
+
+
+$(document).ready(function(){
+  $('.pushpin').pushpin();
+});
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+ 
+});
+
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+});
 
 // $(document).ready(function(){
 //   $('.parallax').parallax();
