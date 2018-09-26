1c7c31ae25affc8a8b864084f4b7b355979d5586
aybaneze <andrea270696@gmail.com>
2018-09-26 15:10:56 -0500

    arreglando el responsive

--- app.js
+++ app.js
@@ -7,33 +7,14 @@ $(document).ready(function(){
   $('.slider').slider();
 });
 
-// $('.pushpin-demo-nav').each(function() {
-//   var $this = $(this);
-//   var $target = $('#' + $(this).attr('data-target'));
-//   $this.pushpin({
-//     top: $target.offset().top,
-//     bottom: $target.offset().top + $target.outerHeight() - $this.height()
-//   });
-// });
-
-
 $(document).ready(function(){
   $('.pushpin').pushpin();
 });
 
 $(document).ready(function(){
   $('.sidenav').sidenav();
- 
 });
 
 $(document).ready(function(){
-  $('.scrollspy').scrollSpy();
+  $('.scrollspy').scrollspy();
 });
-
-// $(document).ready(function(){
-//   $('.parallax').parallax();
-// });
-
-// $(document).ready(function(){
-//   $('.carousel').carousel();
-// });
